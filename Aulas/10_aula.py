efd179b433c0eac39592969f298a60f60e4bb492
Filipe <filipe.uccelli@gmail.com>
2024-12-09 08:04:53 -0300

    Fine tuning

--- Aulas/10_aula.py
+++ Aulas/10_aula.py
@@ -1,125 +1,41 @@
-import openai, os, json
-from dotenv import load_dotenv
-import yfinance as yf
+import json
+import openai
+from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(override=True)
+_ = load_dotenv(find_dotenv())
 
 client = openai.Client()
 
-tools = [
-    {
-        "type": "function",
-        "function": {
-            "name": "retorna_cotacao_historico",
-            "description": "Retorna a cotacão diária histórica para uma acão da bovespa",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "ticker": {
-                        "type": "string",
-                        "description": "O ticker da acao. ex: 'ABEV3' para AMBEV, 'PETRO4' para petrobras e etc.",
-                    },
-                    "period": {
-                        "type": "string", 
-                        "description": 'O periodo que será retornado de dados históricos \
-                                        sendo "1mo" equivalente a 1 mês de dados, \
-                                        "1d" um dia e "1y um ano."',
-                        "enum": ["1d","5d","1mo","3mo","6mo","1y","2y","5y","10y","ytd","max"]
-                    },
-                },
-                "required": ["local"],
-            },
-        },
-    }
-]
+with open("Arquivos/chatbot_respostas.json", encoding="utf8") as f:
+    json_respostas = json.load(f)
 
-def retorna_cotacao_historico(ticker, period="1mo"):
-    ticker = ticker.replace(".SA", "")
-    ticker_obj = yf.Ticker(f"{ticker}.SA")
-    hist = ticker_obj.history(period=period)["Close"]
-    hist.index = hist.index.strftime("%Y-%m-%d")
-    hist = round(hist, 2)
-
-    if len(hist) > 30:
-        slice_size = int(len(hist) / 30)
-        hist = hist.iloc[::-slice_size][::-1]
-
-    return hist.to_json()
-
-funcoes_disponiveis = {
-    "retorna_cotacao_historico": retorna_cotacao_historico
-}
-
-def geracao_texto(messages, model="gpt-3.5-turbo-0125", max_tokens=1000, temperature=0):
-    print(messages)
-    oResponse = client.chat.completions.create(
-        messages=messages,
-        model=model,
-        max_tokens=max_tokens,
-        temperature=temperature,
-        tools=tools,
-        tool_choice='auto',
-    )
-
-    tool_calls = oResponse.choices[0].message.tool_calls
-
-    if tool_calls:
-        messages.append(oResponse.choices[0].message.model_dump(exclude_none=True))
-        for tool_call in tool_calls:
-            func_name = tool_call.function.name
-            function_to_call = funcoes_disponiveis[func_name]
-            func_args = json.loads(tool_call.function.arguments)
-            func_return = function_to_call(**func_args)
-            messages.append({
-                'tool_call_id': tool_call.id,
-                'role': 'tool',
-                'name': func_name,
-                'content': func_return
-            })
-        oResponse = client.chat.completions.create(
-            messages=messages,
-            model='gpt-3.5-turbo-0125',
-        )
-        # messages.append(oResponse.choices[0].message)
-        # print(oResponse.choices[0].message.content)
+with open('Arquivos/chatbot_respostas.jsonl', 'w', encoding="utf8") as f:
+    for entrada in json_respostas:
+        resposta = {
+            'resposta': entrada['resposta'],
+            'categoria': entrada['categoria'],
+            'fonte': 'AsimoBot'
+        }
+        entrada_jsonl = {
+            'messages': [
+                {'role': 'user', 'content': entrada['pergunta']},
+                {'role': 'assistant', 'content': json.dumps(resposta, ensure_ascii=False, indent=2)}
+            ]
+        }
+        json.dump(entrada_jsonl, f, ensure_ascii=False)
+        f.write('\n')
 
 
-    print("Assistant: ", end="")
+file = client.files.create(
+    file=open("Arquivos/chatbot_respostas.jsonl", "rb"),
+    purpose="fine-tune"
+)
 
-    sResponse = oResponse.choices[0].message.content
-    messages.append(oResponse.choices[0].message.model_dump(exclude_none=True))
+breakpoint()
 
-    print(sResponse, end="")
-    
-    return messages
+client.fine_tuning.jobs.create(
+    training_file=file.id,
+    model="gpt-3.5-turbo"
+)
 
-
-messages = []
-while user_message := input("\nUser: "):
-    if not user_message: continue
-
-    messages.append({"role":"user", "content": user_message})
-    messages = geracao_texto(messages)
-
-
-        
-# ticker = "ABEV3"
-# period = "1y"
-
-# historico = retorna_cotacao_historico(ticker, period)
-# print(historico)
-
-
-
-# mensagens = [{'role': 'user', 'content': 'Qual é a cotação da ambev agora'}]
-
-# # Código feio, mané. TODO Fazer melhor
-# resposta = client.chat.completions.create(
-#     messages=mensagens,
-#     model='gpt-3.5-turbo-0125',
-#     tools=tools,
-#     tool_choice='auto'
-# )
-
-
-
+client.fine_tuning.jobs.list()
